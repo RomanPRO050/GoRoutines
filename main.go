963a11f0d7f7a35b4ce4c9ffa6a29c81a899eecc
RomanPRO050 <boev.rs@gmail.com>
2023-06-16 20:14:44 +0300

    GoRoutines_GOMAXPROCS

--- main.go
+++ main.go
@@ -2,15 +2,16 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 	"time"
 )
 
 func main() {
-
+	runtime.GOMAXPROCS(1)
 	go spinner(100 * time.Millisecond)
-	n1 := 10
-	n2 := 15
-	go fmt.Printf("\rFibonacci(%d) = %d\n", n1, fib(n1*2))
+	n1 := 32
+	n2 := 33
+	go fmt.Printf("\rFibonacci(%d) = %d\n", n1, fib(n1))
 	go fmt.Printf("\rFibonacci(%d) = %d\n", n2, fib(n2))
 	time.Sleep(time.Second)
 	//fmt.Printf("\rFibonacci(%d) = %d\n")
